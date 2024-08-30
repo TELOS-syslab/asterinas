b9547010e041daa9aa4bb75e5ea18b5be5752689
Zonghao Zhang <115799498+azongchang@users.noreply.github.com>
2024-08-30 23:09:51 +0800

    Embed tcmalloc-rs into asterinas

--- ostd/src/mm/heap_allocator/mod.rs
+++ ostd/src/mm/heap_allocator/mod.rs
@@ -1,162 +1,24 @@
 // SPDX-License-Identifier: MPL-2.0
 
-mod slab_allocator;
-
-use core::alloc::{GlobalAlloc, Layout};
-
-use align_ext::AlignExt;
-use log::debug;
-use slab_allocator::Heap;
-use spin::Once;
-
-use super::paddr_to_vaddr;
-use crate::{
-    mm::{page::allocator::PAGE_ALLOCATOR, PAGE_SIZE},
-    prelude::*,
-    sync::SpinLock,
-    trap::disable_local,
-    Error,
-};
+use tcmalloc_rs::*;
 
 #[global_allocator]
-static HEAP_ALLOCATOR: LockedHeapWithRescue = LockedHeapWithRescue::new();
+static HEAP_ALLOCATOR: Tcmalloc = Tcmalloc;
 
 #[alloc_error_handler]
 pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
     panic!("Heap allocation error, layout = {:?}", layout);
 }
 
-const INIT_KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 256;
+const INIT_KERNEL_HEAP_SIZE: usize = common::K_PAGE_SIZE * common::K_PRIMARY_HEAP_LEN;
 
 #[repr(align(4096))]
 struct InitHeapSpace([u8; INIT_KERNEL_HEAP_SIZE]);
 
-static mut HEAP_SPACE: InitHeapSpace = InitHeapSpace([0; INIT_KERNEL_HEAP_SIZE]);
+static HEAP_SPACE: InitHeapSpace = InitHeapSpace([0; INIT_KERNEL_HEAP_SIZE]);
 
 pub fn init() {
-    // SAFETY: The HEAP_SPACE is a static memory range, so it's always valid.
-    unsafe {
-        HEAP_ALLOCATOR.init(HEAP_SPACE.0.as_ptr(), INIT_KERNEL_HEAP_SIZE);
-    }
-}
-
-struct LockedHeapWithRescue {
-    heap: Once<SpinLock<Heap>>,
-}
-
-impl LockedHeapWithRescue {
-    /// Creates an new heap
-    pub const fn new() -> Self {
-        Self { heap: Once::new() }
-    }
-
-    /// SAFETY: The range [start, start + size) must be a valid memory region.
-    pub unsafe fn init(&self, start: *const u8, size: usize) {
-        self.heap
-            .call_once(|| SpinLock::new(Heap::new(start as usize, size)));
-    }
-
-    /// SAFETY: The range [start, start + size) must be a valid memory region.
-    unsafe fn add_to_heap(&self, start: usize, size: usize) {
-        self.heap
-            .get()
-            .unwrap()
-            .disable_irq()
-            .lock()
-            .add_memory(start, size);
-    }
-
-    fn rescue_if_low_memory(&self, remain_bytes: usize, layout: Layout) {
-        if remain_bytes <= PAGE_SIZE * 4 {
-            debug!(
-                "Low memory in heap allocator, try to call rescue. Remaining bytes: {:x?}",
-                remain_bytes
-            );
-            // We don't care if the rescue returns ok or not since we can still do heap allocation.
-            let _ = self.rescue(&layout);
-        }
-    }
-
-    fn rescue(&self, layout: &Layout) -> Result<()> {
-        const MIN_NUM_FRAMES: usize = 0x4000000 / PAGE_SIZE; // 64MB
-
-        debug!("enlarge heap, layout = {:?}", layout);
-        let mut num_frames = {
-            let align = PAGE_SIZE.max(layout.align());
-            debug_assert!(align % PAGE_SIZE == 0);
-            let size = layout.size().align_up(align);
-            size / PAGE_SIZE
-        };
-
-    let allocation_start_paddr = {
-        let mut page_allocator = PAGE_ALLOCATOR.get().unwrap().lock();
-        if num_frames >= MIN_NUM_FRAMES {
-            page_allocator
-                .alloc(Layout::from_size_align(num_frames * PAGE_SIZE, PAGE_SIZE).unwrap())
-                .ok_or(Error::NoMemory)?
-        } else {
-            match page_allocator
-                .alloc(Layout::from_size_align(MIN_NUM_FRAMES * PAGE_SIZE, PAGE_SIZE).unwrap())
-            {
-                None => page_allocator
-                    .alloc(Layout::from_size_align(num_frames * PAGE_SIZE, PAGE_SIZE).unwrap())
-                    .ok_or(Error::NoMemory)?,
-                Some(start) => {
-                    num_frames = MIN_NUM_FRAMES;
-                    start
-                }
-            }
-        }
-    };
-    // FIXME: the alloc function internally allocates heap memory(inside FrameAllocator).
-    // So if the heap is nearly run out, allocating frame will fail too.
-    let vaddr = paddr_to_vaddr(allocation_start_paddr);
-
-        // SAFETY: the frame is allocated from FrameAllocator and never be deallocated,
-        // so the addr is always valid.
-        unsafe {
-            debug!(
-                "add frames to heap: addr = 0x{:x}, size = 0x{:x}",
-                vaddr,
-                PAGE_SIZE * num_frames
-            );
-            self.add_to_heap(vaddr, PAGE_SIZE * num_frames);
-        }
-
-        Ok(())
-    }
-}
-
-unsafe impl GlobalAlloc for LockedHeapWithRescue {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let _guard = disable_local();
-
-        let res = self.heap.get().unwrap().lock().allocate(layout);
-        if let Ok((allocation, remain_bytes)) = res {
-            self.rescue_if_low_memory(remain_bytes, layout);
-            return allocation;
-        }
-
-        if self.rescue(&layout).is_err() {
-            return core::ptr::null_mut::<u8>();
-        }
-
-        let res = self.heap.get().unwrap().lock().allocate(layout);
-        if let Ok((allocation, remain_bytes)) = res {
-            self.rescue_if_low_memory(remain_bytes, layout);
-            allocation
-        } else {
-            core::ptr::null_mut::<u8>()
-        }
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        debug_assert!(ptr as usize != 0);
-        self.heap
-            .get()
-            .unwrap()
-            .disable_irq()
-            .lock()
-            .deallocate(ptr, layout)
-    }
+    unsafe { page_heap::init(HEAP_SPACE.0.as_ptr() as usize) };
+    unsafe { central_free_list::init() };
+    unsafe { cpu_cache::init() };
 }
