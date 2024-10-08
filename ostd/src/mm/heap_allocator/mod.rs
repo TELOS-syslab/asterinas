456462b117b9c9f25754d81313183485b8ddf288
azongchang <azongchang@qq.com>
2024-10-08 11:43:35 +0000

    Embed with git

--- ostd/src/mm/heap_allocator/mod.rs
+++ ostd/src/mm/heap_allocator/mod.rs
@@ -2,6 +2,8 @@
 
 use tcmalloc_rs::*;
 
+use crate::early_println;
+
 #[global_allocator]
 static HEAP_ALLOCATOR: Tcmalloc = Tcmalloc;
 
@@ -18,7 +20,11 @@ struct InitHeapSpace([u8; INIT_KERNEL_HEAP_SIZE]);
 static HEAP_SPACE: InitHeapSpace = InitHeapSpace([0; INIT_KERNEL_HEAP_SIZE]);
 
 pub fn init() {
+    early_println!("[tcmalloc] page_heap init.");
     unsafe { page_heap::init(HEAP_SPACE.0.as_ptr() as usize) };
+    early_println!("[tcmalloc] central_free_list init.");
     unsafe { central_free_list::init() };
+    early_println!("[tcmalloc] cpu_cache init.");
     unsafe { cpu_cache::init() };
+    early_println!("[tcmalloc] init done.");
 }
