9d753a551ba3e6666bffd99a66f0c4cd3ff4dc6e
azongchang <azongchang@qq.com>
2024-10-09 12:42:12 +0000

    Try to debug by GDB

--- ostd/src/mm/heap_allocator/mod.rs
+++ ostd/src/mm/heap_allocator/mod.rs
@@ -20,11 +20,9 @@ struct InitHeapSpace([u8; INIT_KERNEL_HEAP_SIZE]);
 static HEAP_SPACE: InitHeapSpace = InitHeapSpace([0; INIT_KERNEL_HEAP_SIZE]);
 
 pub fn init() {
-    early_println!("[tcmalloc] page_heap init.");
     unsafe { page_heap::init(HEAP_SPACE.0.as_ptr() as usize) };
-    early_println!("[tcmalloc] central_free_list init.");
+    early_println!("[tcmalloc] page_heap_base {:x}", HEAP_SPACE.0.as_ptr() as usize);
+    early_println!("[tcmalloc] page_heap_bound {:x}", HEAP_SPACE.0.as_ptr() as usize + INIT_KERNEL_HEAP_SIZE);
     unsafe { central_free_list::init() };
-    early_println!("[tcmalloc] cpu_cache init.");
     unsafe { cpu_cache::init() };
-    early_println!("[tcmalloc] init done.");
 }
