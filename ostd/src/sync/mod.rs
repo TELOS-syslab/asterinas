d699dec4a0e234721f7ab37d9bb5a178bb934854
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-02-05 09:50:22 +0800

    Remove `Arc` guards of locks that have no users

--- ostd/src/sync/mod.rs
+++ ostd/src/sync/mod.rs
@@ -14,18 +14,12 @@ mod wait;
 pub(crate) use self::{guard::GuardTransfer, rcu::finish_grace_period};
 pub use self::{
     guard::{LocalIrqDisabled, PreemptDisabled, WriteIrqDisabled},
-    mutex::{ArcMutexGuard, Mutex, MutexGuard},
+    mutex::{Mutex, MutexGuard},
     rcu::{OwnerPtr, Rcu, RcuOption, RcuReadGuard},
     rwarc::{RoArc, RwArc},
-    rwlock::{
-        ArcRwLockReadGuard, ArcRwLockUpgradeableGuard, ArcRwLockWriteGuard, RwLock,
-        RwLockReadGuard, RwLockUpgradeableGuard, RwLockWriteGuard,
-    },
-    rwmutex::{
-        ArcRwMutexReadGuard, ArcRwMutexUpgradeableGuard, ArcRwMutexWriteGuard, RwMutex,
-        RwMutexReadGuard, RwMutexUpgradeableGuard, RwMutexWriteGuard,
-    },
-    spin::{ArcSpinLockGuard, SpinLock, SpinLockGuard},
+    rwlock::{RwLock, RwLockReadGuard, RwLockUpgradeableGuard, RwLockWriteGuard},
+    rwmutex::{RwMutex, RwMutexReadGuard, RwMutexUpgradeableGuard, RwMutexWriteGuard},
+    spin::{SpinLock, SpinLockGuard},
     wait::{WaitQueue, Waiter, Waker},
 };
 
